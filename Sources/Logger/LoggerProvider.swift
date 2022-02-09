f8ae9f0ad3ccddf907c31ab24bf91ac48acc35f2
ivan <kulikovia.72@icloud.com>
2022-02-09 13:59:08 +0200

    add default LoggerProvider implementation

--- Sources/Logger/LoggerProvider.swift
+++ Sources/Logger/LoggerProvider.swift
@@ -3,3 +3,11 @@ import Foundation
 public protocol LoggerProvider {
     var logsURL: URL { get }
 }
+
+extension LoggerProvider {
+    public var logsURL: URL {
+        FileManager.default
+            .documents
+            .appendingPathComponent("logs")
+    }
+}
